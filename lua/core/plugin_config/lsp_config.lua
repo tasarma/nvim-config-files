64b0793d34b9be925062ec92e5a927bd239ed3c7
tasarma <tasarma@itu.edu.tr>
2023-02-19 21:57:15 +0100

    added LSP and AutoCompletion

---
+++ lua/core/plugin_config/lsp_config.lua
@@ -0,0 +1,16 @@
+require("mason").setup()
+require("mason-lspconfig").setup {
+    ensure_installed = { "lua_ls" },
+}
+
+local on_attach = function(_, _)
+	vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+end
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require("lspconfig").lua_ls.setup{
+	on_attach = on_attach,
+	capabilities = capabilities,
+}
