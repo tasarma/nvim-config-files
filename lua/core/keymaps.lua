9f2b436d48a6676ecd531821cd8d570cd0b3d00e
mahmut tasar <tasarma@itu.edu.tr>
2023-03-10 11:27:16 +0100

    added "set number" to keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,1 +1,6 @@
-vim.keymap.set('n', '<leader>', ':nohlsearch<CR>')
+function set_key_mappings()
+  vim.api.nvim_set_keymap('n', '<leader>n', ':set invnumber<CR>', { noremap = true })
+end
+
+-- Call the function after Neovim is initialized
+vim.cmd([[autocmd VimEnter * lua set_key_mappings()]])
